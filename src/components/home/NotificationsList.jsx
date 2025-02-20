1ee38b4ab52208dde38c9be03f79664db7472964
Kush Mahajan <mahajankush2803@gmail.com>
2025-02-20 18:21:53 +0530

    Changes made

--- src/components/home/NotificationsList.jsx
+++ src/components/home/NotificationsList.jsx
@@ -4,7 +4,7 @@ import NotificationBox from "./NotificationBox";
 const NotificationsList = () => {
   return (
     <div className="space-y-4 max-h-[420px] w-full max-w-full overflow-hidden hover:overflow-y-auto pr-3 thin-scrollbar">
-      <NotificationBox
+      {/* <NotificationBox
         title="Registration Open!"
         message="Don't miss out on securing your spot at the event. Early bird registration ends soon!"
         bgColor="bg-blue-50"
@@ -13,7 +13,7 @@ const NotificationsList = () => {
         title="Schedule Update"
         message="Check out the updated conference schedule on our website!"
         bgColor="bg-yellow-50"
-      />
+      /> */}
       <NotificationBox
         title="Sponsorship Opportunities"
         message="CVIP 2025 is now open for sponsorships! Enhance your brand's visibility by joining us as a sponsor. Explore exclusive packages and benefits."
